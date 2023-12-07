8e328f75715c5e1be468f6e051b4fb9469a6ceb3
miguelfAndrade <miguelandrade.96.18@gmail.com>
2023-12-07 16:13:21 +0000

    Day 2 part 1 done

--- day2/main.cpp
+++ day2/main.cpp
@@ -7,9 +7,9 @@ const int nGreen = 13;
 const int nBlue = 14;
 
 int getGameId(std::string line);
-int possibleIdGame(std::string line);
+int possibleIdGame(std::string gameLine);
 int numberOfSets(std::string line);
-
+int getDigit(std::string set, std::size_t pos);
 
 int main() {
     int sum = 0;
@@ -19,7 +19,7 @@ int main() {
     if (myfile.is_open())
     {
         while (std::getline(myfile, line) )
-        {
+        { 
             sum += possibleIdGame(line);
         }
         myfile.close();
@@ -31,21 +31,60 @@ int main() {
     return 0;
 }
 
-int possibleIdGame(std::string line) {
+int possibleIdGame(std::string gameLine) {
+    std::string line = gameLine;
+
     int id = 0;
-    std::string workLine = line.substr(line.find(":")+2, line.length()-1);
+    int sets = 0;
+    int flag = 1;
 
-    // int semiColonPos = -1;
+    int localRed = 0;
+    int localGreen = 0;
+    int localBlue = 0;
 
-    // for(int i=0; i<line.length(); i++)
-    // {
-    //     std::string firstSet = line.substr(line.find_first_of(";"));
-    // }
+    sets = numberOfSets(line);
 
-    id = getGameId(line);
-    // id = numberOfSets(workLine);
-    // std::cout << "Game ID: " << id << std::endl; 
-    std::cout << "Game " << id << ": " << numberOfSets(workLine) << std::endl; 
+    line = line.substr(line.find(":")+1, line.length()-1);
+
+    for(int i=0; i<sets; i++)
+    {
+        std::size_t semiColonPos = line.find_first_of(";");
+        std::string set = line.substr(0, semiColonPos);
+
+        if(semiColonPos == std::string::npos) 
+        {
+            semiColonPos = line.length()-1;
+            set = line;
+        }
+        
+        std::size_t posRed = set.find(" red");
+        std::size_t posGreen = set.find(" green");
+        std::size_t posBlue = set.find(" blue");
+
+        if(posRed != std::string::npos)
+        {
+            localRed = getDigit(set, posRed);
+        }
+        if(posGreen != std::string::npos)
+        {
+            localGreen = getDigit(set, posGreen);
+        }
+        if(posBlue != std::string::npos)
+        {
+            localBlue = getDigit(set, posBlue);
+        }
+
+        if(localRed > nRed || localGreen > nGreen || localBlue > nBlue) {
+            flag = 0;
+            break;
+        }
+
+        line.erase(0, semiColonPos+1);
+
+    }
+
+    if(flag) id = getGameId(gameLine);
+    
     return id;
 }
 
@@ -68,6 +107,13 @@ int numberOfSets(std::string line) {
         pos = l.find_first_of(";");
         sets++;
     }
-
+    
     return sets+1;
+}
+
+int getDigit(std::string set, std::size_t pos) {
+    int i = pos - 2;
+    if(i<0) i=0;
+
+    return std::stoi(set.substr(i, pos-i));
 }
